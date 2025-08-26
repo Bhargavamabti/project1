0a1cb75161e69435b5464703221ff4e47a2ce993
Bhargavamabti <141702663+Bhargavamabti@users.noreply.github.com>
2025-08-26 19:52:22 +0530

    Add files via upload

---
+++ project1/product.js
@@ -0,0 +1,45 @@
+// EcoThreads Products Data
+const products = [
+  {
+  id: 1,
+  name: "Eco Cotton Dress",
+  image: "dress3.jpg",
+  images: ["dress3.jpg","dress3.jpeg"],
+  price: "$20",
+  short: "Soft organic cotton tee",
+  sizes: ["S","M","L"],
+  sku: "ET-001",
+  material: "100% organic cotton",
+  rating: 4.5,
+  details: ["Machine wash cold","Made in India"]
+
+  },
+  {
+    id: 2,
+    name: "Recycled Olive Green Dress",
+    image: "dress4.png",
+    images: ["dress4.jpg","dress4.jpeg"],
+    price: "$20",
+    short: "Soft organic cotton tee",
+    sizes: ["S","M","L"],
+    sku: "ET-001",
+    material: "100% organic cotton",
+    rating: 4.5,
+    details: ["Machine wash cold","Made in India"]
+  },
+  {
+    id: 3,
+    name: "Bamboo Fiber Dress",
+    price: "$15",
+    image: "dress5.jpeg",
+    images: ["dress5.jpg","dress5.png"],
+    short: "Soft organic cotton tee",
+    sizes: ["S","M","L"],
+    sku: "ET-001",
+    material: "100% organic cotton",
+    rating: 4.5,
+    details: ["Machine wash cold","Made in India"]
+  },
+  
+];
+
